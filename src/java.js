aee4a7b2ae88f12f1e6b1169955e012232149c27
yullittah <142566618+yullittah@users.noreply.github.com>
2023-11-19 19:33:55 +0200

    added static forecast skeleton

--- src/java.js
+++ src/java.js
@@ -62,8 +62,37 @@ function refreshWeather(response) {
     searchCity(searchInput.value);
   }
   
+
+
+
+
   let searchFormElement = document.querySelector("#search-form");
   searchFormElement.addEventListener("submit", handleSearchSubmit);
   
-  searchCity("Hokkaido");
   
+
+function displayForecast(){ 
+
+let forecastElement=document.querySelector("#forecast");
+
+let days= ["Tue", "Wed", "Thur", "Fri", "Sat"];
+
+let forecastHtml="";
+
+days.forEach(function(day){
+    forecastHtml=forecastHtml+
+`  
+    <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-icon">☀️</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong> 15°</strong>
+      </div>    
+        <div class="weather-forecast-temperature"> 9°</div>
+      </div>
+    </div>`;});
+ 
+forecastElement.innerHTML=forecastHtml;}
+searchCity("Hokkaido");
+displayForecast();
